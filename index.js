526e67ae88adba1ea044da58cf72416f63d65cfa
Ashley Richardson <ashley-richardson@lambdastudents.com>
2020-06-25 19:14:18 -0400

    task 1

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
 function createMenuItem(name, cost, category){
-    /* Code here */
+    return {name, cost, category};
 }
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
+console.log(createMenuItem("Cheese Fries", 5, "Sides"));
+console.log(createMenuItem("Sundae", 6, "Sweets"));
+console.log(createMenuItem("Berry Hibiscus", 3, "Teas"));
 
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
@@ -23,6 +26,8 @@ Your method should accept:
 and should return a number. 
 
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
+const burger = {name: "Burger", price: 18, category: "Lunch"};
+
 
 
 
